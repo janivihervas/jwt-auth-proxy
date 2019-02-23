5a51c51eee47fa91c3c20c6e828adde4727d20e5
Jani Viherväs <janivihervas@users.noreply.github.com>
2019-02-23 15:57:00 +0200

    Add server tests and base setup for integration tests (#51)
    * Add server tests and base setup for integration tests
    
    * Fix lint

---
+++ internal/server/server_test.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/janivihervas/authproxy/upstream"
+)
+
+func TestRunHTTP(t *testing.T) {
+	go func() {
+		err := RunHTTP("30000", upstream.Echo{})
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	var err error
+
+	for i := 0; i < 5; i++ {
+		resp, err := http.Get("http://localhost:30000/foo")
+		if err != nil {
+			continue
+		}
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.NoError(t, resp.Body.Close())
+	}
+
+	if err != nil {
+		t.Fatal("Server didn't start", err)
+	}
+}
